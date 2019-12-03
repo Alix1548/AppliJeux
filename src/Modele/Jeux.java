62257c91a405346e8d340a7dd2fa222cbfbde6e9
marie <marie.agba@outlook.fr>
2019-12-03 14:53:17 +0100

    Mise à jour classe Jeux

--- src/Modele/Jeux.java
+++ src/Modele/Jeux.java
@@ -3,4 +3,66 @@ package Modele;
 public class Jeux {
     private String nom;
     private String description;
+    private int duree;
+    private int nbjMax;
+    private int nbjMin;
+
+    public Jeux(String nom, String description, int duree, int nbjMax, int nbjMin) {
+        this.nom = nom;
+        this.description = description;
+        this.duree = duree;
+        this.nbjMax = nbjMax;
+        this.nbjMin = nbjMin;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public int getNbjMax() {
+        return nbjMax;
+    }
+
+    public int getNbjMin() {
+        return nbjMin;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public void setNbjMax(int nbjMax) {
+        this.nbjMax = nbjMax;
+    }
+
+    public void setNbjMin(int nbjMin) {
+        this.nbjMin = nbjMin;
+    }
+
+    @Override
+    public String toString() {
+        return "Jeux{" +
+                "nom='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                ", duree=" + duree +
+                ", nbjMax=" + nbjMax +
+                ", nbjMin=" + nbjMin +
+                '}';
+    }
 }
